d2d2e97d7f2273341e804310b1aed4c6e95581c3
Vanessa Colina <social@vanessacolina.com>
2017-01-22 19:38:46 -0500

    Add files via upload

---
+++ bin/form-2.php
@@ -0,0 +1,20 @@
+<?php	
+	if(empty($_POST['email']))
+	{
+		return false;
+	}
+	
+	$email = $_POST['email'];
+	
+	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+
+	// Create email	
+	$email_subject = "Message from Landing Page Playboardddd.";
+	$email_body = "You have received a new message. \n\n".
+				  "Email: $email \n";
+	$headers = "From: contact@yoursite.com\n";
+	$headers .= "Reply-To: $email";	
+	
+	mail($to,$email_subject,$email_body,$headers); // Post message
+	return true;			
+?>
